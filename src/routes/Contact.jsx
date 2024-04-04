7f3343b3a6dc648db77cf2479aa46dad1fc181c5
VL.dilankarw <dilankarashmika10@gmail.com>
2024-04-04 12:30:16 +0530

    Add Contact Page Header

--- src/routes/Contact.jsx
+++ src/routes/Contact.jsx
@@ -1,7 +1,17 @@
+import Hero from "../components/Hero";
+import Navbar from "../components/Navbar";
+import AboutImg from "../images/Normal.jpg"
+
 function Contact(){
     return(
         <>
-        <h1>This is Contact</h1>
+        <Navbar/>
+        <Hero
+        cName="hero-mid"
+        heroImg={AboutImg}
+        title="Contact"
+        btnClass="hide"
+        />
         </>
     )
 }
