ec14edc488c7be1be2a5748889dfa081785c83e8
VL.dilankarw <dilankarashmika10@gmail.com>
2024-04-05 03:27:03 +0530

    Update About Page

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,18 @@
+import "./AboutUsStyles.css"
+
+function AboutUs(){
+    return(
+        <div className="about-container">
+            <h1>Our History</h1>
+            <p>One of the most iconic views in Luzon Mt Tall boasts a volcano inside a lake inside an island.</p>
+
+            <h1>Our Mission</h1>
+            <p>One of the most iconic views in Luzon Mt Tall boasts a volcano inside a lake inside an island.</p>
+
+            <h1>Our Vision</h1>
+            <p>One of the most iconic views in Luzon Mt Tall boasts a volcano inside a lake inside an island.</p>
+        </div>
+    )
+}
+
+export default AboutUs
--- src/routes/Service.jsx
+++ src/routes/Service.jsx
@@ -1,6 +1,7 @@
 import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
+import Trip from "../components/Trip";
 import AboutImg from "../images/Wild.jpg"
 
 function Service(){
@@ -13,6 +14,7 @@ function Service(){
         title="Service"
         btnClass="hide"
         />
+        <Trip/>
         <Footer/>
         </>
     )
