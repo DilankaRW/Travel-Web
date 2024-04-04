0cc3cc7787a6661427ccfc36c01f043ba7173d1a
VL.dilankarw <dilankarashmika10@gmail.com>
2024-04-05 00:33:14 +0530

    Update Trip Section

--- src/components/Trip.jsx
+++ src/components/Trip.jsx
@@ -1,10 +1,34 @@
 import "./TripStyles.css";
+import TripData from "./TripData";
+import Trip1 from "../images/cover.jpg"
+import Trip2 from "../images/cover1.jpg"
+import Trip3 from "../images/cover2.jpg"
+import Trip4 from "../images/cover3.jpg"
 
 function Trip(){
     return(
         <div className="trip">
             <h1>Recent Trips</h1>
             <p>You can discover unique destinations using Google Maps.</p>
+            <div className="tripcard">
+                <TripData
+                image={Trip1}
+                heading="Trip in Indonesia"
+                text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans."
+                />
+
+<TripData
+                image={Trip2}
+                heading="Trip in Indonesia"
+                text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans."
+                />
+
+<TripData
+                image={Trip3}
+                heading="Trip in Indonesia"
+                text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans."
+                />
+            </div>
         </div>
     )
 }
