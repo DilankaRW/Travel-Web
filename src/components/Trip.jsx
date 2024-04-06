99defaf55f6d29d5d351d88a3e923f712b20cb07
VL.dilankarw <dilankarashmika10@gmail.com>
2024-04-06 23:35:44 +0530

    Update Trip Image Variables

--- src/components/Trip.jsx
+++ src/components/Trip.jsx
@@ -1,8 +1,8 @@
 import "./TripStyles.css";
 import TripData from "./TripData";
-import Trip1 from "../images/bentot_beach.jpg"
-import Trip2 from "../images/nine_arch_bridge.jpg"
-import Trip3 from "../images/Regular.jpg"
+import Trip_image1 from "../images/bentot_beach.jpg"
+import Trip_image2 from "../images/nine_arch_bridge.jpg"
+import Trip_image3 from "../images/Regular.jpg"
 
 function Trip(){
     return(
@@ -11,19 +11,19 @@ function Trip(){
             <p>You can discover unique destinations using Google Maps.</p>
             <div className="tripcard">
                 <TripData
-                image={Trip1}
+                image={Trip_image1}
                 heading="Trip in Indonesia"
                 text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans."
                 />
 
                 <TripData
-                image={Trip2}
+                image={Trip_image2}
                 heading="Trip in Indonesia"
                 text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans."
                 />
 
                 <TripData
-                image={Trip3}
+                image={Trip_image3}
                 heading="Trip in Indonesia"
                 text="Indonesia, officially the Republic of Indonesia, is a country in Southeast Asia and Oceania between the Indian and Pacific oceans."
                 />
